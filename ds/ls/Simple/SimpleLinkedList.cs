478e473ab56fb3f82fcec77989bf47f56f8bff1c
teng2xbe <teng2xbe@gmail.com>
2017-07-17 16:51:16 -0700

    simple

---
+++ ds/ls/Simple/SimpleLinkedList.cs
@@ -0,0 +1,21 @@
+﻿namespace ls.Simple
+{
+    public class SimpleLinkedList
+    {
+        private SimpleNode head;
+        public SimpleLinkedList()
+        {
+            head = null;
+        }
+
+        public void AppendNode(SimpleNode node)
+        {
+            if (head == null)
+                head = node;
+            else
+            {
+                head.NextNode = node;                
+            }
+        }
+    }
+}
---
+++ ds/ls/Simple/SimpleNode.cs
@@ -0,0 +1,10 @@
+﻿namespace ls.Simple
+{
+    public class SimpleNode
+    {
+        public int Data { get; set; }
+        public SimpleNode NextNode { get; set; }
+
+
+    }
+}
