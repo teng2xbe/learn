641288d8e7636b298219d0dc03fd4637149dc204
teng2xbe <teng2xbe@gmail.com>
2017-07-18 16:05:52 -0700

    test

---
+++ ds/ls.Tests/Simple/SimpleFixture.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using ls.Simple;
+using NUnit.Framework;
+
+namespace ls.Tests.Simple
+{
+    [TestFixture]
+    public class SimpleFixture
+    {
+        [Test]
+        public void AppendLast_ShouldAddNodeAtTheEnd()
+        {
+            var ls = new SimpleLinkedList();
+            ls.AppendLast(new SimpleNode { Data = 1 });
+            ls.AppendLast(new SimpleNode { Data = 2 });
+            ls.AppendLast(new SimpleNode { Data = 3 });
+
+            var nodeList = ls.GetList();
+            Assert.AreEqual(1, nodeList[0].Data);
+            Assert.AreEqual(2, nodeList[1].Data);
+            Assert.AreEqual(3, nodeList[2].Data);
+        }
+
+        [Test]
+        public void InsertFirst_ShouldInsertAtTheStartOfList()
+        {
+            var ls = new SimpleLinkedList();
+            ls.InsertFirst(new SimpleNode { Data = 1 });
+            ls.InsertFirst(new SimpleNode { Data = 2 });
+            ls.InsertFirst(new SimpleNode { Data = 3 });
+
+            var nodeList = ls.GetList();
+            Assert.AreEqual(3, nodeList[0].Data);
+            Assert.AreEqual(2, nodeList[1].Data);
+            Assert.AreEqual(1, nodeList[2].Data);
+        }
+    }
+}
