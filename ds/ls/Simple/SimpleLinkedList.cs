11f78e42d585ec5ab70a5f0d2116f8a5c42ba563
teng2xbe <teng2xbe@gmail.com>
2017-07-18 15:56:46 -0700

    add last and insert first

--- ds/ls/Simple/SimpleLinkedList.cs
+++ ds/ls/Simple/SimpleLinkedList.cs
@@ -1,4 +1,6 @@
-﻿namespace ls.Simple
+﻿using System.Collections.Generic;
+
+namespace ls.Simple
 {
     public class SimpleLinkedList
     {
@@ -8,13 +10,59 @@
             head = null;
         }
 
-        public void AppendNode(SimpleNode node)
+        public IList<SimpleNode> GetList()
+        {
+            var result = new List<SimpleNode>();
+            
+            return GetList(result, head);
+        }
+
+        private IList<SimpleNode> GetList(IList<SimpleNode> list, SimpleNode currentNode)
+        {
+            if (currentNode == null)
+            {
+                return list;
+            }
+            list.Add(currentNode);
+
+            return GetList(list, currentNode.NextNode);            
+        }
+
+        public void AppendLast(SimpleNode node)
         {
             if (head == null)
+            {
                 head = node;
+            }
             else
             {
-                head.NextNode = node;                
+                AppendLast(head, node);
+            }
+        }
+
+        private void AppendLast(SimpleNode current, SimpleNode node)
+        {
+            if (current.NextNode == null)
+            {
+                current.NextNode = node;
+            }
+            else
+            {
+                AppendLast(current.NextNode, node);
+            }
+        }
+
+        public void InsertFirst(SimpleNode node)
+        {
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                var tempNode = head;
+                head = node;
+                node.NextNode = tempNode;
             }
         }
     }
