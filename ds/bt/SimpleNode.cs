8a696862a088f8d8f895e44282d2b05ead7e55e6
teng2xbe <teng2xbe@gmail.com>
2017-07-20 11:15:08 -0700

    bt

---
+++ ds/bt/SimpleNode.cs
@@ -0,0 +1,9 @@
+﻿namespace bt
+{
+    public class SimpleNode
+    {
+        public int Data { get; set; }
+        public SimpleNode RigthNode { get; set; }
+        public SimpleNode LeftNode { get; set; }
+    }
+}
