dfd66fedd4db5d68795c70ce368cd59b9e46ed7b
teng2xbe <teng2xbe@gmail.com>
2017-07-20 14:33:33 -0700

    bt

--- ds/bt.Tests/SimpleTreeFixture.cs
+++ ds/bt.Tests/SimpleTreeFixture.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace bt.Tests
 {
+    [TestFixture]
     public class SimpleTreeFixture
     {
+        [Test]
+        public void CreateTreeWithInitialHeadNode()
+        {
+            
+            var bt = new SimpleTree(new SimpleNode {Data = 100});
+            Assert.AreEqual(100, bt.GetNode(100).Data);
+        }
+
+        [Test]
+        public void AddSorted_AddedTheNodeInOrder()
+        {
+            var bt = new SimpleTree(new SimpleNode { Data = 100 });
+            bt.AddSorted(new SimpleNode {Data = 75});
+            bt.AddSorted(new SimpleNode { Data = 155 });
+            bt.AddSorted(new SimpleNode { Data = 125 });
+            bt.AddSorted(new SimpleNode { Data = 15 });
+            bt.AddSorted(new SimpleNode { Data = 85 });
+            Assert.AreEqual(100, bt.GetNode(100).Data);
+        }
     }
 }
---
+++ ds/bt/SimpleTree.cs
@@ -0,0 +1,77 @@
+﻿namespace bt
+{
+    public class SimpleTree
+    {
+        private SimpleNode head;
+
+        public SimpleTree()
+        {
+            head = null;
+        }
+
+        public SimpleTree(SimpleNode head)
+        {
+            this.head = head;
+        }
+
+        public SimpleNode GetNode(int data)
+        {
+            return GetNode(head, data);
+        }
+
+        private SimpleNode GetNode(SimpleNode current, int data)
+        {
+            if (data > head.Data)
+            {
+                return GetNode(head.RigthNode, data);
+            }
+
+            if (data < head.Data)
+            {
+                return GetNode(head.LeftNode, data);
+            }
+            
+            return current;
+        }
+
+        public void AddSorted(SimpleNode node)
+        {
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                AddSorted(head, node);
+            }            
+        }
+
+
+        private void AddSorted(SimpleNode current, SimpleNode node)
+        {
+            if (node.Data > current.Data)
+            {
+                if (current.RigthNode == null)
+                {
+                    current.RigthNode = node;
+                }
+                else
+                {
+                    AddSorted(current.RigthNode, node);
+                }                
+            }
+            else if (node.Data < current.Data)
+            {
+                if (current.LeftNode == null)
+                {
+                    current.LeftNode = node;
+                }
+                else
+                {
+                    AddSorted(current.LeftNode, node);
+                }                
+            }
+            
+        }
+    }
+}
